12c5990a2411acfda8fe6eacf31e4628ecf9bb48
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-10-06 22:45:39 +0300

    Add hash and register route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+
+const User = require('../models/User');
 
 // route  POST api/users
 // desc   Register a user
@@ -17,13 +20,38 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name: name,
+        email: email,
+        password: password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("test user route/hash ");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
