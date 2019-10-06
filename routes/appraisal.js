f1d01e10cfa85cf942f43a6dcf134750410f9526
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-10-06 21:54:51 +0300

    Connect MongoDB

--- routes/appraisal.js
+++ routes/appraisal.js
@@ -1,32 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // route  GET api/appraisals
 // desc   Get all users appraisals
 // access Private
-router.get('/', (req, res) => {
-  res.send('Get all appraisals')
+router.get("/", (req, res) => {
+  res.send("Get all appraisals");
 });
 
 // route  POST api/appraisals
 // desc   Add new appraisal
 // access Private
-router.post('/', (req, res) => {
-  res.send('Add appraisals')
+router.post("/", (req, res) => {
+  res.send("Add appraisals");
 });
 
 // route  PUT api/appraisal/:id
 // desc   Update appraisal
 // access Private
-router.put('/:id', (req, res) => {
-  res.send('Update appraisal')
+router.put("/:id", (req, res) => {
+  res.send("Update appraisal");
 });
 
 // route  DELETE api/appraisal/:id
 // desc   Update appraisal
 // access Private
-router.delete('/:id', (req, res) => {
-  res.send('delete appraisal')
+router.delete("/:id", (req, res) => {
+  res.send("delete appraisal");
 });
 
 module.exports = router;
